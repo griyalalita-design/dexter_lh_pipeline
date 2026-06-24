e058cb8c8d4c5d572868adbc540733c3271772aa
griyalalita-design <griya.lalita@ninjavan.co>
2026-06-24 15:09:40 +0700

    Update settings.py

--- pipeline/config/settings.py
+++ pipeline/config/settings.py
@@ -85,7 +85,7 @@ GSHEET = {
 }
 # ── Metabase ─────────────────────────────────────────────────
 METABASE_CONFIG = {
-    "lm": {
+    "lh": {
         "iv_poa": {
             "url": "https://metabase.ninjavan.co/api/card/123978/query/json",
             "report_type": "lm",
@@ -93,28 +93,28 @@ METABASE_CONFIG = {
                 {"id":"d6472180-efcd-48f3-a7bb-e6210b4a32ac","type":"date/single","value":"end_date","target":["variable",["template-tag", "exp_dep_end"]]},
                 {"id":"522e86b9-f7ea-45c6-ac0a-92ab57ca33e8","type":"string/=","value":["COMPLETED", "ARRIVED"],"target":["variable",["dimension", ["template-tag", "status"]]]},
                 {"id":"6f7bd3bb-7a76-49ae-9f5e-ee78d1531118","type":"date/single","value":"start_date","target":["variable",["template-tag", "exp_dep_start"]]},
-                {"id":"e8a405ca-e034-4f56-a4ca-6f454452a5ce","type":"string/=","value":["LAND_HAUL"],"target":["dimension", ["template-tag", "movement_type"]]},
+                # {"id":"e8a405ca-e034-4f56-a4ca-6f454452a5ce","type":"string/=","value":["LAND_HAUL"],"target":["dimension", ["template-tag", "movement_type"]]},
             
             ],
-            "shipper_params_template": {
-                "agg_fsbd": [
-                    {"id":"054d3fb3-9f04-4d21-a56d-5f046d53281a","type":"string/=","value": "agg_fsbd","target":["dimension",["template-tag","shipper_id"]]},
+            # "shipper_params_template": {
+            #     "agg_fsbd": [
+            #         {"id":"054d3fb3-9f04-4d21-a56d-5f046d53281a","type":"string/=","value": "agg_fsbd","target":["dimension",["template-tag","shipper_id"]]},
 
-                ],
-                "b2b_all_b2c_cc": [
-                    {"id":"054d3fb3-9f04-4d21-a56d-5f046d53281a","type":"string/=","value": "b2b_all_b2c_cc","target":["dimension",["template-tag","shipper_id"]]},
+            #     ],
+            #     "b2b_all_b2c_cc": [
+            #         {"id":"054d3fb3-9f04-4d21-a56d-5f046d53281a","type":"string/=","value": "b2b_all_b2c_cc","target":["dimension",["template-tag","shipper_id"]]},
         
-                ],
-                "laz_shop_tt": [
-                    {"id":"6d243afd-9693-4b71-bd9d-43ba83ae3112","type":"string/=","value":[341107,216977],"target":["dimension",["template-tag","parent_id"]]},
+            #     ],
+            #     "laz_shop_tt": [
+            #         {"id":"6d243afd-9693-4b71-bd9d-43ba83ae3112","type":"string/=","value":[341107,216977],"target":["dimension",["template-tag","parent_id"]]},
 
-                ],
-            },
+            #     ],
+            # },
           },  
 
         "n0_completion": {
             "url": "https://metabase.ninjavan.co/api/card/122260/query/json",
-            "report_type": "lm",
+            "report_type": "lh",
             "common_params_template": [
                 {"id":"987f60d5-d9cb-4a60-ac0d-982ed5a60a2f","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_5"]]},
                 # {"id":"f304224f-3506-4723-8be1-128aa5d268c3","type":"number/=","value":[15],"target":["variable",["template-tag","cutoff_time"]]},
@@ -134,153 +134,88 @@ METABASE_CONFIG = {
                     {"id":"a3692197-cccd-458b-828c-9adec640d018","type":"string/=","value":"b2b_all_b2c_cc","target":["dimension",["template-tag","shipper_id"]]},
                     {"id":"f304224f-3506-4723-8be1-128aa5d268c3","type":"number/=","value":[12],"target":["variable",["template-tag","cutoff_time"]]},
                 ],
-                "b2b_dry_cc_next": [
-                    {"id":"a3692197-cccd-458b-828c-9adec640d018","type":"string/=","value":"b2b_dry_cc_next","target":["dimension",["template-tag","shipper_id"]]},
-                    {"id":"f304224f-3506-4723-8be1-128aa5d268c3","type":"number/=","value":[15],"target":["variable",["template-tag","cutoff_time"]]},
-                ],
-                "tt": [
-                    {"id":"8cc079bf-483f-4ff4-bb30-8504ee5f8d16","type":"string/=","value":[7474545],"target":["dimension",["template-tag","parent_id"]]},
-                    {"id":"f304224f-3506-4723-8be1-128aa5d268c3","type":"number/=","value":[15],"target":["variable",["template-tag","cutoff_time"]]},
-                ],
             },
         },
 
-        "n1_completion": {
-            "url": "https://metabase.ninjavan.co/api/card/122260/query/json",
-            "report_type": "lm",
-            "common_params_template": [
-                {"id":"987f60d5-d9cb-4a60-ac0d-982ed5a60a2f","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_5"]]},
-                {"id":"f304224f-3506-4723-8be1-128aa5d268c3","type":"number/=","value":[15],"target":["variable",["template-tag","cutoff_time"]]},
-                {"id":"6637429a-e441-440b-b55c-0d12d4e8a187","type":"number/=","value":[1],"target":["variable",["template-tag","exclude_xcld"]]},
-                {"id":"9b12da54-2115-48ad-bb4c-a1f601f5d8f4","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_3"]]},
-                {"id":"0f4ec3a3-aef7-4da6-aa53-de9b05465d08","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_7"]]},
-                {"id":"9bcc17a7-e96a-4068-b3a0-0e3cc6a6b548","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_2"]]},
-                {"id":"10dcf155-48f2-489d-ad91-b9375327ebe4","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
-                {"id":"95aab3ce-921b-49ca-af4f-2843d926772a","type":"date/all-options","value":"period_str","target":["dimension",["template-tag","dest_hub_datetime"]]},
-                {"id":"0e55fe86-ef1e-4cf5-b3fe-c77a76961bbe","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_4"]]},
-                {"id":"0c6c599e-8675-4f20-94df-bd51ae0648b7","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_1"]]},
-                {"id":"ae99fb31-181b-404f-8de7-ff6d22ccdeff","type":"number/=","value":[0],"target":["variable",["template-tag","whitelisted_day_6"]]},
-                {"id":"9a4ba0ba-f4ec-4212-b8d0-63505d846bc9","type":"number/=","value":[1],"target":["variable",["template-tag","4pl_flag"]]}
-            ]
-        },
-      
-        "completion_within_timeslot": {
-            "url": "https://metabase.ninjavan.co/api/card/124081/query/json",
-            "report_type": "lm",
-            "common_params_template": [
-                {"id":"370cf3c3-3e21-4af9-98f4-58ddce2fb3cc","type":"string/=","value":"b2b_sds_dry_cold_prem","target":["dimension",["template-tag","sds_instant"]]},
-                {"id":"45c4636d-b5ea-4e91-ac1c-d39eb785f808","type":"string/=","value":"b2b_sds_dry_cold_reg","target":["dimension",["template-tag","sds_reg"]]},
-                {"id":"8837dec8-26c8-41f8-9016-2bf6f9eaa178","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
-                {"id":"952ddad7-da28-44ef-bb00-f20871d02a55","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]},
-                {"id":"786ee2aa-67f6-4494-b153-0c999b0080eb","type":"category","value":["month"],"target":["variable", ["template-tag", "aggr"]]}
-            ],
-        },
 
-        "lnd_b2b_all_b2c_cc": {
-            "url": "https://metabase.ninjavan.co/api/card/122299/query/json",
-            "report_type": "lm",
-            "common_params_template": [
-                {"id":"d49289f0-e6be-cdae-aaba-c025c53fe61e","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
-                {"id":"e6a1e9c8-8f83-9dfd-fecb-832d67512759","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
-                {"id":"e017c9c6-0345-5b57-2d2c-a99a375ec2cb","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]},
-                {"id":"b43a47f7-fe04-417d-8bce-ef5f111b8fa7","type":"string/=","value":"b2b_all_b2c_cc","target":["dimension",["template-tag","shipper_id"]]}
-            ]
-        },
+        # "lnd_b2b_all_b2c_cc": {
+        #     "url": "https://metabase.ninjavan.co/api/card/44744/query/json",
+        #     "report_type": "lh",
+        #     "common_params_template": [
+        #       {"id":"458aa3b7-1c44-ce99-badb-176ad6013196","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
+        #       {"id":"6954221a-2bb3-e6ff-0fdd-22eb3a2960ab","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
+        #       {"id":"ce4438d1-ebcb-89c6-1997-cbbc50c70402","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]},
+        #       {"id":"b2c822b6-27f4-3fca-5f1d-a7291f9b102e","type":"category","value":["LAND_HAUL"],"target":["dimension",["template-tag","shipment_type"]]}
+        #     ]
+        # },
 
-        "no_rsvn_completed": {
+        "no_rsvn_completed_b2b_all_b2c_cc": {
             "url": "https://metabase.ninjavan.co/api/card/122256/query/json",
-            "report_type": "lm",
+            "report_type": "lh",
             "common_params_template": [
                 {"id": "190fb3a4-e6cb-4b4e-a78b-f4acb7cc5448", "type": "category", "value": "month", "target": ["variable", ["template-tag", "aggr"]]},
                 {"id": "26473b49-9801-4240-ade1-6c07b7851c2a", "type": "date/single", "value": "start_end", "target": ["variable", ["template-tag", "start_date"]]},
                 {"id": "8d72da7f-6a48-4384-a283-a1c81db37e2d", "type": "date/single", "value": "end_date", "target": ["variable", ["template-tag", "end_date"]]},
-                {"id": "f1fca7d5-bddb-42ce-9771-1f17b2c6a1ec", "type": "string/=", "value": "driver_list", "target": ["dimension", ["template-tag", "route_driver_type"]]},
-                {"id": "f72285e9-4c7f-4b94-9b22-9cf8c929946f", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "nv_not_liable"]]},
+                {"id": "f1fca7d5-bddb-42ce-9771-1f17b2c6a1ec", "type": "string/=", "value": ["Linehaul Driver"], "target": ["dimension", ["template-tag", "route_driver_type"]]},
+                # {"id": "f72285e9-4c7f-4b94-9b22-9cf8c929946f", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "nv_not_liable"]]},
                 {"id": "ecfc3da2-aca6-4303-bb42-aa3f9a21810d", "type": "string/contains", "value": ["B2BR"], "target": ["dimension", ["template-tag", "pickup_tags"]], "options": {"case-sensitive": False}},
 
             ],
             "shipper_params_template": {
-                "b2br_key_shipper": [
-                    {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "string/=", "value": "b2br_key_shipper", "target": ["dimension", ["template-tag", "shipper_id"]]},
-                    # {"id": "452ee39f-7548-48f9-88aa-5348c9addd86", "type": "string/=","value":["7474545"],"target":["variable",["template-tag","parent_id_coalesce"]]},
-                ],
-                "sds_cc": [
-                    {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "string/=", "value": "sds_cc", "target": ["dimension", ["template-tag", "shipper_id"]]},
+                "b2b_all_b2c_cc": [
+                    {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "string/=", "value": "b2b_all_b2c_cc", "target": ["dimension", ["template-tag", "shipper_id"]]},
+
                 ],
             },
         },
 
-        "poda_val_sho_laz": {
-            "url": "https://metabase.ninjavan.co/api/card/122274/query/json",
-            "report_type": "lm",
+        "shipment_compliance": {
+            "url": "https://metabase.ninjavan.co/api/card/82724/query/json",
+            "report_type": "lh",
             "common_params_template": [
-                {"id":"bd6a4239-0385-8972-2a17-0dae89833d44","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
-                {"id":"f5d97598-03a0-4812-5396-9a7e48dff486","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
-                {"id":"eea9787b-32e5-4f45-a751-8205f3fa5853","type":"string/=","value":[341107,216977],"target":["dimension",["template-tag","parent_id"]]},
-                {"id":"cd2c4e8c-cba2-d597-7cd1-04acca8e86ca","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]}
+                {"id":"71e58020-6764-cf05-040b-67d83febf1a8","type":"string/=","value":["Completed","At Transit Hub","Closed","Transit"],"target":["dimension",["template-tag","status"]]},
+                {"id":"180a98eb-40a8-6228-8e04-54d8509a818c","type":"category","value":["day"],"target":["variable",["template-tag","aggr"]]},
+                {"id":"e2a4c28b-bfb7-f7b5-eada-261b5f755eec","type":"string/=","value":["LAND_HAUL"],"target":["dimension",["template-tag","shipment_type"]]},
+                {"id":"9cbc5903-59a6-b2f5-d73f-3e59ffe00c02","type":"date/all-options","value":"period_str","target":["dimension",["template-tag","orig_shipment_close_datetime"]]}
             ]
         },
 
-        "pu_rot": {
+        "into_hub_compliance": {
             "url": "https://metabase.ninjavan.co/api/card/122256/query/json",
             "report_type": "lm",
             "common_params_template": [
-                {"id":"8d72da7f-6a48-4384-a283-a1c81db37e2d","type":"date/single","value":"end_date","target":["variable",["template-tag","end_date"]]},
-                {"id":"f1fca7d5-bddb-42ce-9771-1f17b2c6a1ec","type":"string/=","value":"driver_list","target":["dimension",["template-tag","route_driver_type"]]},
-                {"id":"6980e48f-126e-48d9-a0d3-da79bbd63751","type":"string/=","value":"b2br_key_shipper","target":["dimension",["template-tag","shipper_id"]]},
-                {"id":"190fb3a4-e6cb-4b4e-a78b-f4acb7cc5448","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
-                {"id":"ecfc3da2-aca6-4303-bb42-aa3f9a21810d","type":"string/contains","value":["B2BR"],"options":{"case-sensitive":False},"target":["dimension",["template-tag","pickup_tags"]]},
-                {"id":"26473b49-9801-4240-ade1-6c07b7851c2a","type":"date/single","value":"start_date","target":["variable",["template-tag","start_date"]]}
+                {"id":"36dcdc26-b630-cf67-57ca-a1e36336804b","type":"category","value":["day"],"target":["variable",["template-tag","aggr"]]},
+                {"id":"af9f1c12-8827-6aad-418c-30fc4cca99ee","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
+                {"id":"2da317e4-9fc0-13c0-2a71-059d435e64ed","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]},
+                {"id":"82c5651d-4539-780a-1344-41c6779c502b","type":"category","value":["LAND_HAUL"],"target":["dimension",["template-tag","shipment_type"]]}
             ]
         },
 
-        "td6": {
-            "url": "https://metabase.ninjavan.co/api/card/122261/query/json",
-            "report_type": "fm",
-            "common_params_template": [
-                # {"id":"4e3825b2-b673-4bc2-850a-6653df4b0e52","type":"id","value":[9691851,9691856,9691857,9691878,9691882,9691900,9691927,9691947,9691966,9692052,9692053],"target":["dimension",["template-tag","shipper_id"]]},
-                {"id":"adef369b-762a-ad76-c4e0-cbd4e2bc53ef","type":"category","value":["month"],"target":["variable",["template-tag","aggr"]]},
-                {"id":"6eb39d69-fa95-fdcf-3ef7-7944e8945867","type":"date/all-options","value":"period_str","target":["dimension",["template-tag","d6_cutoff_date"]]},
-                {"id":"1c3908b4-daad-4747-b1eb-f23c291acfcc","type":"number/=","value":[1],"target":["variable",["template-tag","exclude_shipper_5x_attempt"]]}
-            ],
-            "shipper_params_template": {
-                "shop_laz": [
-                    {"id":"08abe0d9-db20-4471-bf32-2ff223991971","type":"string/=","value":[341107,216977],"target":["dimension",["template-tag","parent_id"]]},
-                ],
-                "aggregator": [
-                    {"id":"4e3825b2-b673-4bc2-850a-6653df4b0e52","type":"id","value":"aggregator","target":["dimension",["template-tag","shipper_id"]]},
-                ],
-                "4pl": [
-                    {"id": "93050463-b83a-4b79-9176-c9baa78b5365", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "4pl_flag"]]},
-                ],
-            },
-        },
-
-        "rdo_rtd_b2b": {
-            "url": "https://metabase.ninjavan.co/api/card/116132/query/json",
-            "report_type": "fm",
-            "common_params_template": [
-                {"id":"0c07a704-c667-4468-a4e5-76e76367de31","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
-                {"id":"5dfa2a9b-1df1-4b15-abcc-b44bf7f0fe16","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]}
-            ],
-        },
+        # "rdo_rtd_b2b": {
+        #     "url": "https://metabase.ninjavan.co/api/card/116132/query/json",
+        #     "report_type": "fm",
+        #     "common_params_template": [
+        #         {"id":"0c07a704-c667-4468-a4e5-76e76367de31","type":"date/single","value":"start_date","target":["variable",["template-tag","start"]]},
+        #         {"id":"5dfa2a9b-1df1-4b15-abcc-b44bf7f0fe16","type":"date/single","value":"end_date","target":["variable",["template-tag","end"]]}
+        #     ],
+        # },
     }
 }
 # ── Archieved File ─────────────────────────────────────────────────────
-ARCHIVE_CONFIG = {
-    "tracker": {
-        "source_file_id": "10jwwERVKLvdrk7tkmqVXeZHGp3Q1lV_2IjFsc6fXQhQ",
-        "target_folder_id": "18BwwadT0kgyPgvdmOg3b7CP29zqrTFB4",
-    },
-    "sanggahan": {
-        "source_file_id": "1q1CkYFiZQKRvfYDjZGOJmqNOndbH3hd7Du_Pq0Wn7AU",
-        "target_folder_id": "12d8VANvDb2earFpAJOgQLj4kW1ITZm_U",
-    },
-    "converter": {
-        "source_file_id": "1Sn2HisZcT81duWuWtKpVx_E_8192XeFIwtXrrDoSpGQ",
-        "target_folder_id": "1Dx2EJoddhhdSqJs5qkNG8rWB9ydCc1Sl",
-    },
-}
+# ARCHIVE_CONFIG = {
+#     "tracker": {
+#         "source_file_id": "10jwwERVKLvdrk7tkmqVXeZHGp3Q1lV_2IjFsc6fXQhQ",
+#         "target_folder_id": "18BwwadT0kgyPgvdmOg3b7CP29zqrTFB4",
+#     },
+#     "sanggahan": {
+#         "source_file_id": "1q1CkYFiZQKRvfYDjZGOJmqNOndbH3hd7Du_Pq0Wn7AU",
+#         "target_folder_id": "12d8VANvDb2earFpAJOgQLj4kW1ITZm_U",
+#     },
+#     "converter": {
+#         "source_file_id": "1Sn2HisZcT81duWuWtKpVx_E_8192XeFIwtXrrDoSpGQ",
+#         "target_folder_id": "1Dx2EJoddhhdSqJs5qkNG8rWB9ydCc1Sl",
+#     },
+# }
 
 # ── Pipeline Schedule ─────────────────────────────────────────
 # Ini untuk referensi GitHub Actions
